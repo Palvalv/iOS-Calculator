c90ed794c1e79f968672e4c3d8a88bef841c55e2
Palvalv <101711875+Palvalv@users.noreply.github.com>
2023-05-08 13:10:24 +0200

    First Commit

---
+++ CalculatorApp/CalculatorApp/AppDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate.swift
+//  CalculatorApp
+//
+//  Created by Pablo Alvarez on 06/05/23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        // Setup
+        setupView()
+        
+        return true
+    }
+    
+    // MARK: - Private methods
+    
+    private func setupView() {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+
+}
+
